47d9ff492718c559a3ec400a30b313a7372cde66
Pol Alfonso <pol.alfonso@urv.cat>
2026-03-09 13:05:34 +0100

    pruebas "funcionan"

--- ia-practica1-busqueda-informada/src/test/java/ia/practica1/AppTest.java
+++ ia-practica1-busqueda-informada/src/test/java/ia/practica1/AppTest.java
@@ -1,16 +1,17 @@
 package ia.practica1;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+
+import org.junit.Test;
 
 import ia.practica1.exceptions.SinSolucion;
 import ia.practica1.formal.Carretera;
 import ia.practica1.formal.Estado;
 import ia.practica1.formal.Mapa;
 import ia.practica1.formal.Solucion;
-import ia.practica1.heuristicas.DistanciaManhattan;
-
-import org.junit.Test;
-import java.io.FileNotFoundException;
+import ia.practica1.heuristicas.Euclidea;
 
 /**
  * Unit test for simple App.
@@ -38,13 +39,44 @@ public class AppTest
                 "../data/default-map-10x10.csv",
                 new Estado (0,0, Carretera.NACIONAL),
                 new Estado (9,9, Carretera.NACIONAL),
-                new DistanciaManhattan ()
+                new Euclidea ()
             );
 
             solucion = mapa.bestFirst();
             
             mapa.mostrarMapaConCamino( solucion.getCamino() );
             this.mostrarReporte(solucion, "BF");
+            System.out.println("L'heuristica a l'estat inicial es:"+mapa.getHeuristica().evaluar(mapa.getInicial()));
+            
+        } catch ( FileNotFoundException e ) {
+            System.out.println("Buscando en: " + new java.io.File(".").getAbsolutePath());
+            e.printStackTrace();
+        } catch ( SinSolucion e ) {
+            fail("Tiene Solucion pero no lo encuentra");
+        }
+    }
+
+    // A* amb heuristica constante para prueba
+    @Test
+    public void test02 () {
+        Mapa mapa;
+        Solucion solucion;
+
+        try {
+            mapa = new Mapa (
+                10,
+                10,
+                "../data/default-map-10x10.csv",
+                new Estado (0,0, Carretera.NACIONAL),
+                new Estado (9,9, Carretera.NACIONAL),
+                new Euclidea ()
+            );
+
+            solucion = mapa.AStar();
+            
+            mapa.mostrarMapaConCamino( solucion.getCamino() );
+            this.mostrarReporte(solucion, "A*");
+            System.out.println("L'heuristica a l'estat inicial es:"+mapa.getHeuristica().evaluar(mapa.getInicial()));
             
         } catch ( FileNotFoundException e ) {
             System.out.println("Buscando en: " + new java.io.File(".").getAbsolutePath());
