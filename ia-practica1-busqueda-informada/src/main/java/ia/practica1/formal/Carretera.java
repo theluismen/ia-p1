1b5dc2fa715da42c24012675480256b5f24a86b3
Pol Alfonso <pol.alfonso@urv.cat>
2026-03-09 01:23:09 +0100

    añadida función devolver valor carretera

--- ia-practica1-busqueda-informada/src/main/java/ia/practica1/formal/Carretera.java
+++ ia-practica1-busqueda-informada/src/main/java/ia/practica1/formal/Carretera.java
@@ -15,5 +15,19 @@ public enum Carretera {
     public char getSimbolo () {
         return simbolo;
     }
+
+    public float getValor(){
+        switch (simbolo){
+            case 'A':
+                return 0.5f;
+            case 'N':
+                return 1f;
+            case 'C':
+                return 2f;
+            default:
+                return 0;
+            
+        }
+    }
     
 }
