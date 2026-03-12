f7473e080b32d65ddf8f2f37219ad662f91f2f93
Pol Alfonso <pol.alfonso@urv.cat>
2026-03-12 11:34:53 +0100

    cambio nombre

--- ia-practica1-busqueda-informada/src/main/java/ia/practica1/heuristicas/DiferenciaCarreteras.java
+++ ia-practica1-busqueda-informada/src/main/java/ia/practica1/heuristicas/DiferenciaCarreteras.java
@@ -2,7 +2,7 @@ package ia.practica1.heuristicas;
 
 import ia.practica1.formal.Estado;
 
-public class HeuristicaCarreteras extends Heuristica {
+public class DiferenciaCarreteras extends Heuristica {
 
     @Override
     public float evaluar(Estado sucesor){
